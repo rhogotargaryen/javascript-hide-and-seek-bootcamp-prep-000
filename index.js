7f4d238224b4392c1a89573ad3a2023828f328fe
Derek Haller <dhaller.personal@gmail.com>
2017-09-27 16:29:06 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function getFirstSelector(selector) {
+   return document.querySelector(`${selector}`);
+ }
+
+ function nestedTarget() {
+   return document.querySelector("#nested .target");
+ }
+
+ function increaseRankBy(n) {
+   var nList = document.querySelectorAll("ul.ranked-list li");
+   for (var i = 0; i < nList.length; i++) {
+     nList[i].innerHTML = parseInt(nList[i].innerHTML) + n;
+   }
+ }
+
+function deepestChild() {
+  var container = document.querySelector("#grand-node");
+  var childContainer = container.children[0];
+  while (childContainer) {
+    container = childContainer;
+    childContainer = container.children[0];
+  }
+  return container;
+  }
