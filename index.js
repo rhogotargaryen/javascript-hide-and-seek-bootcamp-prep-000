c85aa75963cb969ce0f65517662cdd49211c3f5c
Derek Haller <dhaller.personal@gmail.com>
2017-09-27 16:12:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
--function getFirstSelector(selector) {
- -  return document.querySelector(`${selector}`);
- -}
- -
- -function nestedTarget() {
- -  return document.querySelector("#nested .target");
- -}
- -
- -function increaseRankBy(n) {
- -  var nList = document.querySelectorAll("ul.ranked-list li");
- -  for (var i = 0; i < nList.length; i++) {
- -    nList[i].innerHTML = parseInt(nList[i].innerHTML) + n;
- -  }
- -} 
+function getFirstSelector(selector) {
+   return document.querySelector(`${selector}`);
+ }
+ 
+ function nestedTarget() {
+   return document.querySelector("#nested .target");
+ }
+ 
+ function increaseRankBy(n) {
+   var nList = document.querySelectorAll("ul.ranked-list li");
+   for (var i = 0; i < nList.length; i++) {
+     nList[i].innerHTML = parseInt(nList[i].innerHTML) + n;
+   }
+ } 
